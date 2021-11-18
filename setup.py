17e96040ab146a23fe1a1a604b0469fba199ad80
Tim Vink <tim.vink@allianzdirect.nl>
2021-11-18 16:59:59 +0100

    bump to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup_args = {
     "name": "precommit_nbconvert_rename",
-    "version": "0.0.2",
+    "version": "0.1.0",
     "packages": find_packages(),
     "install_requires": ["jupyter-client", "nbconvert", "pre-commit"],
     "author": "Tim Vink",
